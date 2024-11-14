85c25586f2ecf3e93ac9920c0cfd6581d34893a8
Ramiro Matta <137447213+RamiroMatta9@users.noreply.github.com>
2024-11-13 20:16:04 -0500

    Update botones.js

--- js/botones.js
+++ js/botones.js
@@ -15,6 +15,10 @@ document.addEventListener("DOMContentLoaded", function () {
                 handleContactButton(e, estaAbierto, 'https://api.whatsapp.com/send/?phone=%2B51967315653&text=Buen+d%C3%ADa%2C+estoy+interesado+en+obtener+consultoria+financiera+de+XTRATEGO.');
             } else if (action === "capacitacion") {
                 handleContactButton(e, estaAbierto, 'https://api.whatsapp.com/send/?phone=%2B51967315653&text=Buen+d%C3%ADa%2C+estoy+interesado+en+la+capacitaci%C3%B3n+de+XTRATEGO.');
+            }else if (action === "digitalizacion") {
+                    handleContactButton(e, estaAbierto, 'https://api.whatsapp.com/send/?phone=%2B51967315653&text=Buen%20d%C3%ADa,%20me%20gustar%C3%ADa%20m%C3%A1s%20informaci%C3%B3n%20sobre%20la%20digitalizaci%C3%B3n%20y%20automatizaci%C3%B3n%20que%20ofrece%20XTRATEGO.');               
+            }else if (action === "gestion-tributaria") {
+                    handleContactButton(e, estaAbierto, 'https://api.whatsapp.com/send/?phone=%2B51967315653&text=Buen%20d%C3%ADa,%20me%20gustar%C3%ADa%20informaci%C3%B3n%20sobre%20el%20servicio%20de%20gesti%C3%B3n%20tributaria%20y%20fiscal%20que%20ofrece%20XTRATEGO.');                   
             } else if (action === "proximamente") {
                 e.preventDefault();
                 Swal.fire({
